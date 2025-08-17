8cae4adbe68c88977a0481ec404acd56d3034465
Handsome Mofokeng <handsomem23@hotmail.com>
2025-08-18 01:09:09 +0200

    Upgrade Gradle and update libraries
    - Upgrade Android Gradle Plugin to 8.12.0
    - Upgrade Kotlin to 2.2.10
    - Upgrade coreKtx to 1.17.0
    - Upgrade junitVersion to 1.3.0
    - Upgrade espressoCore to 3.7.0
    - Upgrade kotlinxCollectionsImmutable to 0.4.0
    - Upgrade lifecycleRuntimeKtx to 2.9.2
    - Upgrade composeBom to 2025.08.00
    - Upgrade navigationRuntimeKtx to 2.9.3
    - Upgrade navigationCompose to 2.9.3
    - Update compileSdk to 36
    - Upgrade Gradle wrapper to 8.14.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "africa.digitalhusters.composewhatsapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "africa.digitalhusters.composewhatsapp"
